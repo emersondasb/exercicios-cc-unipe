7038554767305598ef2cff591d5bf8bcffaf3557
Emerson Bezerra <emersonbioinfo@gmail.com>
2019-09-30 21:08:17 -0300

    primeira questao da lista de exercicios 2

---
+++ lab-desenvolvimento-algoritmos/pratica2.1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	int lista[5];
+	int numero, i;
+	
+	puts("Digite 5 números teclando ENTER entre um e outro: ");
+	
+	for(i=0; i<5;)
+	{
+		numero = getc(stdin);
+		
+		if(numero != '\n')
+		{
+			lista[i] = numero;
+			i++;
+		} 
+	}
+
+	puts("\nOs números digitados na ordem crescente de índice é: ");
+	for(i=0; i<5 ; i++)
+	{
+		printf("%c ", lista[i]);
+	}
+	
+	puts("\n\nOs números digitados na ordem decrescente de índice é: ");
+	for(i=4; i>=0 ; i--)
+	{
+		printf("%c ", lista[i]);
+	}
+}
+

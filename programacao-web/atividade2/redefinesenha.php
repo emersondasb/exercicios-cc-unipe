1b7292519d8f8752b80a4cfa0c02b6b465e78c12
Emerson Bezerra <emersonbioinfo@gmail.com>
2019-10-02 17:37:05 -0300

    correcao na hora de verificar senhas no cadastro e na redefinicao

--- programacao-web/atividade2/redefinesenha.php
+++ programacao-web/atividade2/redefinesenha.php
@@ -1,25 +1,37 @@
 <?php
 
-    session_start();
+session_start();
 
-    echo "<b>O usuário atual é:</b> " . $_SESSION['usuarioSessao'] . "<br/>";
-    echo "<b>A senha atual é:</b> " . $_SESSION['senhaSessao'] . "<br/>";
+echo "<b>O usuário atual é:</b> " . $_SESSION['usuarioSessao'] . "<br/>";
+echo "<b>A senha atual é:</b> " . $_SESSION['senhaSessao'] . "<br/>";
 ?>
 
 <html>
-<body>
-<br/>
-<form action="validacadastro.php" method="POST">
-    Novo usuário: <input type="text" name="usuario" required/><br/>
-    Nova senha: <input type="password" id="senha" required/><br/>
-    Redigite a nova senha: <input type="password" id="senhaRepete" name="senha" required/><br/>
-    <input type="submit" value="Enviar" onclick="confirmaRedefinicao()"/>
-</form>
 
-<script>
-    function confirmaRedefinicao(){
-        alert("Informações atualizadas com sucesso!");
-    }
-</script>
+<body>
+    <br />
+    <form action="validacadastro.php" method="POST" onsubmit="return senhaConfirma()">
+        Novo usuário: <input type="text" name="usuario" required /><br />
+        Nova senha: <input type="password" id="senha" required /><br />
+        Redigite a nova senha: <input type="password" id="senhaRepete" name="senha" required /><br />
+        <input type="submit" value="Enviar" onclick="confirmaRedefinicao()" />
+    </form>
+
+
+    <script>
+        function senhaConfirma() {
+            var senhaDigitada = document.getElementById('senha').value;
+            var confirmaSenha = document.getElementById('senhaRepete').value;
+
+            if (senhaDigitada != confirmaSenha) {
+                alert("As senhas digitadas não coincidem. Digite novamente!");
+                return false;
+            } else {
+                alert("Informações atualizadas com sucesso!");
+            }
+        }
+    </script>
+
 </body>
+
 </html>
